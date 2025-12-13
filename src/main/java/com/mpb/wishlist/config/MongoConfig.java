dceacc7062c7e17999a03081ed1762219fdcf5f4
mario <mario.bressan@hotmai.com>
2025-12-13 17:44:59 -0300

    add MongoDB configuration

---
+++ src/main/java/com/mpb/wishlist/config/MongoConfig.java
@@ -0,0 +1,28 @@
+package com.mpb.wishlist.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.MongoDatabaseFactory;
+import org.springframework.data.mongodb.core.convert.DefaultDbRefResolver;
+import org.springframework.data.mongodb.core.convert.DefaultMongoTypeMapper;
+import org.springframework.data.mongodb.core.convert.MappingMongoConverter;
+import org.springframework.data.mongodb.core.convert.MongoCustomConversions;
+import org.springframework.data.mongodb.core.mapping.MongoMappingContext;
+
+@Configuration
+public class MongoConfig {
+
+    @Bean
+    public MappingMongoConverter mappingMongoConverter(MongoDatabaseFactory factory,
+                                                       MongoMappingContext context,
+                                                       MongoCustomConversions conversions) {
+        var dbRefResolver = new DefaultDbRefResolver(factory);
+        var converter = new MappingMongoConverter(dbRefResolver, context);
+
+        converter.setCustomConversions(conversions);
+
+        converter.setTypeMapper(new DefaultMongoTypeMapper(null));
+
+        return converter;
+    }
+}
